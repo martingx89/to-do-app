89956cf44a94c25749d9d56b36b10c80f74f638c
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 05:27:52 +0000

    Import components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Hero from './components/Hero/Hero';
+import SearchForm from './components/SearchForm/SearchForm';
+
 const App = () => {
   return (
     <div>
-      <h1>My first React App</h1>
-      <p>A simple to-do app, with lists, columns and card</p>
-      <form>
-        <input type="text" />
-        <button>Search</button>
-      </form>
+      <Hero />
+      <SearchForm />
     </div>
   );
 };

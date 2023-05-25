99dc280c0ae261451b6340bc38be736ed1ad71e1
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 09:40:42 +0000

    Use children

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,14 @@
-import styles from './SearchForm.module.scss'
+import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 
 const SearchForm = () => {
-    return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder='Search'/>
-            <Button text='Search'/>
-        </form>
-    );
+  return (
+    <form className={styles.searchForm}>
+      <TextInput placeholder='Search' />
+      <Button>Search</Button>
+    </form>
+  );
 };
 
 export default SearchForm;

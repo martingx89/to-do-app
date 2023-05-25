60c1f4253038659e75ce254e27a549056ec568a4
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 08:06:25 +0000

    Add Column component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,25 +1,22 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
-    return (
-      <div className={styles.List}>
-          <header className={styles.header}>
-              <h2 className={styles.title}>Things to do<span>soon!</span></h2>
-          </header>
-          <p className={styles.description}>Interesting things I want to check out</p>
-          <section className={styles.columns}>
-              <article>
-                  <h2>Books</h2>
-              </article>
-              <article>
-                  <h2>Movies</h2>
-              </article>
-              <article>
-                  <h2>Games</h2>
-              </article>
-          </section>
-      </div>
-    );
-  };
-  
-  export default List;
+  return (
+    <div className={styles.List}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          Things to do<span>soon!</span>
+        </h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+        <Column title='Books' />
+        <Column title='Movies' />
+        <Column title='Games' />
+      </section>
+    </div>
+  );
+};
+
+export default List;

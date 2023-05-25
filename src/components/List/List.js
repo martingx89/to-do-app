71e66a07d3c1604cf638dfc9bc4e10e3ed0eec50
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 10:43:43 +0000

    Add function to List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,12 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 
 const List = () => {
+  const columns = [
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' },
+  ];
+
   return (
     <div className={styles.List}>
       <header className={styles.header}>
@@ -11,9 +17,9 @@ const List = () => {
       </header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-        <Column icon='book' title='Books' />
-        <Column icon='film' title='Movies' />
-        <Column icon='gamepad' title='Games' />
+        {columns.map((column) => (
+          <Column key={column.id} title={column.title} icon={column.icon} />
+        ))}
       </section>
     </div>
   );

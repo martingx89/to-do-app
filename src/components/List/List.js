64db00d134a071abfc8576ca2f10cd69785d9c29
Marcin Adamiak <marcin_89@outlook.com>
2023-05-25 19:05:16 +0200

    Add useState and form

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,22 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
-  const columns = [
+  const [columns, setColumns] = useState([
     { id: 1, title: 'Books', icon: 'book' },
     { id: 2, title: 'Movies', icon: 'film' },
     { id: 3, title: 'Games', icon: 'gamepad' },
-  ];
+  ]);
+
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
 
   return (
     <div className={styles.List}>
@@ -21,6 +31,10 @@ const List = () => {
           <Column key={column.id} title={column.title} icon={column.icon} />
         ))}
       </section>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={value} onChange={(e) => setValue(e.target.value)} />
+        <button>Add column</button>
+      </form>
     </div>
   );
 };

e6d6e670ad66856d17446e3c94cad2e22b9c9766
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 06:02:52 +0000

    Import styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 ReactDOM.render(<App />, document.querySelector('#root'));

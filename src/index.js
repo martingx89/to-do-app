53ee46e0beea66c9fdc93fe739eec3ebe0e39822
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 08:09:30 +0000

    Add FontAwesome support

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));

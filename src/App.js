423eb4fcec7bafd98df8e2a54ae8b147d20c9379
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-05-25 05:48:17 +0000

    Add List component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './components/Hero/Hero';
 import SearchForm from './components/SearchForm/SearchForm';
+import List from './components/List/List';
 
 const App = () => {
   return (
     <div>
       <Hero />
       <SearchForm />
+      <List />
     </div>
   );
 };

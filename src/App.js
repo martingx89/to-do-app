d2548b2db009a694c1ca254dc3a756bdcb86f259
Marcin Adamiak <marcin_89@outlook.com>
2023-06-01 19:13:12 +0200

    Use router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import NavBar from './components/NavBar/NavBar';
 import Home from './Home/Home';
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
-        <About />
-        <Favorite />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/favorite' element={<Favorite />}></Route>
+        </Routes>
       </Container>
     </main>
   );

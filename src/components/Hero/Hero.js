12aaf412d3ab6d9006ccea261640bc3611e5142b
Marcin Adamiak <marcin_89@outlook.com>
2023-06-01 18:51:31 +0200

    Use PageTitle

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,10 @@
+import { PageTitle } from '../PageTitle/PageTitle';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <h1 className={styles.title}>My first React App</h1>
+      <PageTitle>My first React App</PageTitle>
       <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
     </div>
   );

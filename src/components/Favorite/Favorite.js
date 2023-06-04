4a850b4d69444828a23070a511895609380a81ef
Marcin Adamiak <marcin_89@outlook.com>
2023-06-04 21:58:26 +0200

    Add favorite content

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,4 +1,5 @@
 import PageTitle from '../PageTitle/PageTitle';
+import Card from '../Card/Card';
 import styles from './Favorite.module.scss';
 import { useSelector } from 'react-redux';
 import { getFliteredFavoriteCards } from '../../redux/store';
@@ -7,10 +8,22 @@ export const Favorite = () => {
   const favCards = useSelector(getFliteredFavoriteCards);
   console.log(favCards);
 
+  if (favCards.length !== 0) {
+    return (
+      <div className={styles.favorite}>
+        <PageTitle>Favorite</PageTitle>
+        <ul className={styles.cards}>
+          {favCards.map((card) => (
+            <Card key={card.id} {...card} isFavorite={card.isFavorite} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
   return (
-    <div>
+    <div className={styles.favorite}>
       <PageTitle>Favorite</PageTitle>
-      <p className={styles.subtitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, facilis!</p>
+      <p className={styles.favorite_subtitle}>No Cards</p>
     </div>
   );
 };

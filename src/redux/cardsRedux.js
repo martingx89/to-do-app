1df2f2ebe92a4a441956b22b53663b046fa52309
Marcin Adamiak <marcin_89@outlook.com>
2023-06-05 18:59:44 +0200

    Prepare remove action

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -8,10 +8,12 @@ export const getFliteredFavoriteCards = (state) => {
 // actions
 const createActionName = (actionName) => `app/cards/${actionName}`;
 const ADD_CARD = createActionName('ADD_CARD');
+const REMOVE_CARD = createActionName('REMOVE_CARD');
 const TOGGLE_CARD_FAVORITE = createActionName('TOGGLE_CARD_FAVORITE');
 
 // action creators
 export const addCard = (payload) => ({ type: ADD_CARD, payload });
+export const removeCard = (payload) => ({ type: REMOVE_CARD, payload });
 
 export const toggleCardFavorite = (payload) => ({
   type: TOGGLE_CARD_FAVORITE,
@@ -22,6 +24,8 @@ const cardsReducer = (statePart = [], action) => {
   switch (action.type) {
     case ADD_CARD:
       return [...statePart, { ...action.payload, id: shortid() }];
+    case REMOVE_CARD:
+      return [...statePart, { ...action.payload, id: shortid }];
     case TOGGLE_CARD_FAVORITE:
       return statePart.map((card) => (card.id === action.payload ? { ...card, isFavorite: !card.isFavorite } : card));
     default:
